f981d2c653b1c2b594177b120e520a07347e0502
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-08-16 20:11:41 +0200

    create Mongoose schema for database

---
+++ models.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+  Title: {type: String, required: true},
+  Description: {type: String, required: true},
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+    Birth: Number,
+    Death: Number
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean,
+  ReleaseYear: Number
+});
+
+let userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+// creates collections db. movies and db.users (automatically pluralized!)
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
