33c8c5c0f7ebe5ccffb2601bc7379dd8b507d04e
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-09-26 17:23:20 +0200

    enable auth. for movies endpoint

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/', (req, res) => {
 })
 
 // Return a list of ALL movies to the user
-app.get('/movies', /* passport.authenticate('jwt', { session: false }), */(req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(200).json(movies);
